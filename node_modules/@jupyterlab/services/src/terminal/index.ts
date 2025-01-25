f59c626e5e1cf00f5a7a7dd1cd1093e50124e5bf
yuuu14 <yu.xiao.14@outlook.com>
2025-01-25 10:55:37 +0800

    add jupyterlab support

--- node_modules/@jupyterlab/services/src/terminal/index.ts
+++
@@ -1,8 +0,0 @@
-// Copyright (c) Jupyter Development Team.
-// Distributed under the terms of the Modified BSD License.
-
-import * as Terminal from './terminal';
-import * as TerminalAPI from './restapi';
-
-export * from './manager';
-export { Terminal, TerminalAPI };
--- node_modules/@lumino/coreutils/src/index.node.ts
+++
@@ -1,8 +0,0 @@
-/*
- * Copyright (c) Jupyter Development Team.
- * Distributed under the terms of the Modified BSD License.
- */
-
-export * from './index.common';
-export * from './random.node';
-export * from './uuid.node';
